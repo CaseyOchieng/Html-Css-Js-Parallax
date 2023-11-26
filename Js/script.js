f11034d3644242a8bbd145516b274e3ce03b2f3d
casey <caseyochieng46@gmail.com>
2023-11-26 19:37:41 +0300

    added css

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,4 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+//Here we have created a function that will change the background color of the text and the leaf.
